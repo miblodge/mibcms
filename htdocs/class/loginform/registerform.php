da63888eb590ca1123a4983417922607f1ca9ff4
antonchanning <squ4r3-us3r>
2014-10-18 00:35:41 +0100

    Default html template for  registration forms.

---
+++ htdocs/class/loginform/registerform.php
@@ -0,0 +1,9 @@
+	<div class="register-form">
+	<form action="index.php" method="post">
+		<div class="register-form-name"><label>Email:</label> <input type="text" name="email" value="" /></div>
+		<div class="register-form-name"><label>Username:</label> <input type="text" name="username" value="" /></div>
+		<div class="register-form-pswd"><label>Password:</label> <input type="password" name="password" value="" /></div>
+		<div class="register-form-pswd-conf"><label>Password Again:</label> <input type="password" name="password-conf" value="" /></div>
+		<div class="register-form-button"><input type="submit" name="register" value="Register" /></div>
+	</form>
+	</div>
