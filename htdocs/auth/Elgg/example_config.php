91ddd08547d72670da770e6ac3af78fa60824b03
antonchanning <squ4r3-us3r>
2014-10-17 23:57:37 +0100

    Example config file for Elgg authentication.

---
+++ htdocs/auth/Elgg/example_config.php
@@ -0,0 +1,7 @@
+<?php
+	// Copy this file to config.php and fill in correct values.
+	define('ELGG_DB_HOST', 'localhost');
+	define('ELGG_DB_NAME', 'mysql_db_name');
+	define('ELGG_DB_USER', 'mysql_user');
+	define('ELGG_DB_PASSWORD', 'password');
+	define('ELGG_DB_PREFIX', 'elgg');
