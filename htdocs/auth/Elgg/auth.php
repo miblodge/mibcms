90812e9c67dd4c04f6ac39acaa4861f458e8fec6
antonchanning <squ4r3-us3r>
2014-10-17 23:48:07 +0100

    Config settings for authenticating users via an Elgg installation.

---
+++ htdocs/auth/Elgg/auth.php
@@ -0,0 +1,83 @@
+<?php
+	require_once('./auth/'.MIB_AUTH.'/config.php');
+	includeClass('class_authentication.php');
+
+	class ElggAuth extends Authentication {
+		public $elgg_user_data = null;
+
+		function __construct() {
+			$cookieValue = $_COOKIE["Elgg"];
+
+			$this->setElggUserData($cookieValue);
+
+			parent::__construct();
+		}
+
+		function __get($name) {
+			switch($name) {
+				case 'id':
+				case 'userid':
+					return $this->elgg_user_data['id'];
+					break;
+				case 'username':
+					return $this->elgg_user_data['username'];
+					break;
+				case 'name':
+				case 'display_name':
+					return $this->elgg_user_data['name'];
+					break;
+				case 'banned':
+					$banned = ($this->elgg_user_data['banned'] != 'no');
+					return $banned;
+				case 'logged_in':
+					return isset($this->elgg_user_data['id']);
+					break;	
+				default:
+					// Important, for all other undefined values, try the parent class...
+					return parent::__get($name);
+					break;
+			}
+		}
+
+		function setElggUserData($cookieValue) {
+			$elggUser = array();
+			//      $md5cookie = md5($cookieValue);
+			$users = ELGG_DB_PREFIX . 'users_entity';
+			$users_sess = ELGG_DB_PREFIX . 'users_sessions';
+
+			$rows = $this->elggDbQuery("select $users.guid id, $users.username username, $users.name name, " .
+						"$users.email email, $users.banned banned, $users_sess.session " .
+						"from $users left join $users_sess on $users.last_action = $users_sess.ts " .
+						"where $users_sess.session = '$cookieValue'");
+
+			if(count($rows) > 0) {
+				$elggUser = $rows[0];
+			} else {
+				$this->err[] = 'Couldnt get user';
+			}
+			$this->elgg_user_data = $elggUser;
+		}
+
+		// -----------------------------------------------------------------------
+		function elggDbQuery($query)
+		{
+			if(!$cnx = mysql_connect(ELGG_DB_HOST, ELGG_DB_USER, ELGG_DB_PASSWORD)) {
+				$this->err[] = 'Could not connect to database: ' . mysql_error();
+			}
+			
+			if(!mysql_select_db(ELGG_DB_NAME, $cnx)) {
+				$this->err[] = 'Could not select database: ' . mysql_error();
+			}
+			
+			if(!$rs = mysql_query($query, $cnx)) {
+				$this->err[] = 'Could not execute query: ' . mysql_error();
+			}
+			$rows = array();
+			while ($row = mysql_fetch_assoc($rs)) {
+				$rows[] = $row;
+			}
+			return $rows;
+		}
+	}
+
+	$auth = new ElggAuth();
