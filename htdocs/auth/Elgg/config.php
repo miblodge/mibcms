a0d946a005914f1cf7e38a1aee6131c3bae2e15c
antonchanning <squ4r3-us3r>
2014-10-17 23:46:26 +0100

    Config settings for authenticating users via an existing Elgg installation.

---
+++ htdocs/auth/Elgg/config.php
@@ -0,0 +1,6 @@
+<?php
+	define('ELGG_DB_HOST', 'localhost');
+	define('ELGG_DB_NAME', 'devadmin_chanzelgg');
+	define('ELGG_DB_USER', 'devadmin_chanz');
+	define('ELGG_DB_PASSWORD', 'r34lm-74!');
+	define('ELGG_DB_PREFIX', 'elgg');
