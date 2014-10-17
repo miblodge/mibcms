2b17e5a1c95f50c75ee18439148a38fd9f45bec5
antonchanning <squ4r3-us3r>
2014-10-18 00:34:47 +0100

    Default html template for login forms.

---
+++ htdocs/class/loginform/loginform.php
@@ -0,0 +1,7 @@
+	<div class="login-form">
+	<form action="index.php" method="post">
+		<div class="login-form-name"><label>Username:</label> <input type="text" name="username" value="" /></div>
+		<div class="login-form-pswd"><label>Password:</label> <input type="password" name="password" value="" /></div>
+		<div class="login-form-button"><input type="submit" name="login" value="Login" /></div>
+	</form>
+	</div>
